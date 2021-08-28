5193c7eb22ad530274b285ea8cc57f2d5d924989
MyeongjunKim <ilikerunning@naver.com>
2021-08-29 04:42:15 +0900

    feat: styled-components button

--- chap04/styling/src/App.js
+++ chap04/styling/src/App.js
@@ -6,20 +6,39 @@ import { viewStyles, textStyles } from './styles';
 import { Header, Contents, Footer } from './components/Layout';
 // Shadow 태그 불러오기
 import ShadowBox from './components/ShadowBox';
+// 스타일드 컴포넌트로 만든 버튼 불러오기
+import styled from 'styled-components/native';
+import Button from './components/Button';
+
+const Container = styled.View`
+    
+    background-color: #ffffff;
+    align-items: center;
+    justify-content: center;
+`;
 
 const App = () => {
     return (
         <View style={viewStyles.container}> 
-
             <Header />
             <Contents />
             <Footer /> 
             <ShadowBox />
 
-        </View>
+            <Container>
+                <Button title="new Button by styled component" />
+            </Container>
+
+        </View>        
+        
     );
 };
 
-
+        // <View style={viewStyles.container}> 
+        //     <Header />
+        //     <Contents />
+        //     <Footer /> 
+        //     <ShadowBox />
+        // </View>
 
 export default App;
